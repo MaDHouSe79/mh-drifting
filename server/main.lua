1c88d69ee52f4c01e6b3d1c08be28d366e9147d8
MaDHouSe79 <madhouse1979@gmail.com>
2022-08-04 11:32:16 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,12 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem(Config.driftItem, function(source)
-    TriggerClientEvent('qb-drifting:client:drifmode', source)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local player_cars = MySQL.Sync.fetchScalar('SELECT COUNT(*) FROM player_vehicles WHERE citizenid = ? AND plate = ?', {Player.PlayerData.citizenid, plate})
+    if player_cars >= 1 then
+        TriggerClientEvent('qb-drifting:client:drifmode', src)
+    else
+        TriggerClientEvent('QBCore:Notify', src, "This is not your vehicle", "error")
+    end
 end)
