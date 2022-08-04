85243b010d9228f95c7cb788ea56a002f21b0d6b
MaDHouSe79 <madhouse1979@gmail.com>
2022-08-04 12:07:53 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -5,6 +5,7 @@ local Translations = {
         drift_mode_off = "Disable",
         enjoy_sideways = "Enjoy driving sideways!",
         enjoy_standard = "Enjoy driving standard!",
+        not_the_owner  = "Je bent geen eigenaar van dit voertuig",
     },
 }
 
