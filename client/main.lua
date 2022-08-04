4f9cda534a439348d50bed547e9f83bb78a0e599
MaDHouSe79 <madhouse1979@gmail.com>
2022-08-04 11:55:34 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,42 +1,51 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local driftMode = false
 
---[[
-RegisterKeyMapping('undrift', "Set Vehicle as undriftable", 'keyboard', 'F') 
-RegisterCommand('undrift', function()
-    if IsPedInAnyVehicle(PlayerPedId()) then
-        if not driftMode then
-            TriggerEvent('qb-drifting:client:drifmode')
-        end
-    end
-end, false)
-]]--
-
 RegisterNetEvent('qb-drifting:client:drifmode', function()
+
 	local ped = PlayerPedId()
 	if IsPedInAnyVehicle(ped) then
 		local vehicle = GetVehiclePedIsIn(ped, false)
-		if (GetPedInVehicleSeat(vehicle, -1) == ped) then
-			local modifier = 1
-			if GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDragCoeff") > 90 then driftMode = true else driftMode = false end
-			if driftMode then modifier = -1 end
-			for index, value in ipairs(Config.handleMods) do
-				SetVehicleHandlingFloat(vehicle, "CHandlingData", value[1], GetVehicleHandlingFloat(vehicle, "CHandlingData", value[1]) + value[2] * modifier)
-			end
-			if driftMode then
-				QBCore.Functions.Notify(Lang:t('info.drift_mode') .. " " .. Lang:t('info.drift_mode_off').. " " .. Lang:t('info.enjoy_standard'), "success", 1000)
-			else
-				QBCore.Functions.Notify(Lang:t('info.drift_mode') .. " " .. Lang:t('info.drift_mode_on').. " " .. Lang:t('info.enjoy_sideways'), "success", 1000)
-			end
-			if GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDragCoeff") < 90 then
-				SetVehicleEnginePowerMultiplier(vehicle, 0.0)
-			else
-				if GetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveBiasFront") == 0 then
-					SetVehicleEnginePowerMultiplier(vehicle, 190.0)
-				else
-					SetVehicleEnginePowerMultiplier(vehicle, 100.0)
+		local props = QBCore.Functions.GetVehicleProperties(vehicle)
+		print(props.plate)
+		QBCore.Functions.TriggerCallback('qb-drifting:server:isOwner', function(isOwner)
+			if isOwner then
+
+				if (GetPedInVehicleSeat(vehicle, -1) == ped) then
+					local modifier = 1
+
+					if GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDragCoeff") > 90 then
+						driftMode = true 
+					else 
+						driftMode = false 
+					end
+
+					if driftMode then modifier = -1 end
+
+					for index, value in ipairs(Config.handleMods) do
+						SetVehicleHandlingFloat(vehicle, "CHandlingData", value[1], GetVehicleHandlingFloat(vehicle, "CHandlingData", value[1]) + value[2] * modifier)
+					end
+					if driftMode then
+						QBCore.Functions.Notify(Lang:t('info.drift_mode') .. " " .. Lang:t('info.drift_mode_off').. " " .. Lang:t('info.enjoy_standard'), "success", 1000)
+					else
+						QBCore.Functions.Notify(Lang:t('info.drift_mode') .. " " .. Lang:t('info.drift_mode_on').. " " .. Lang:t('info.enjoy_sideways'), "success", 1000)
+					end
+					if GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDragCoeff") < 90 then
+						SetVehicleEnginePowerMultiplier(vehicle, 0.0)
+					else
+						if GetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveBiasFront") == 0 then
+							SetVehicleEnginePowerMultiplier(vehicle, 190.0)
+						else
+							SetVehicleEnginePowerMultiplier(vehicle, 100.0)
+						end
+					end
 				end
+
+			else
+				driftMode = false
+				QBCore.Functions.Notify("You are not the owner", "error", 1000)
 			end
-		end
+		end, props.plate)
+
 	end
 end)
