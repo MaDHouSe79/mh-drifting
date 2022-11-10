6a1b7cdfe08d3a580fffd8032ebdf51d935d4db3
MaDHouSe79 <madhouse1979@gmail.com>
2022-11-10 04:07:18 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
-
-description 'QB-Garages'
-version '1.0.0'
+author 'MaDHouSe'
+description 'MH Drifting'
+version '1.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
@@ -11,17 +11,11 @@ shared_scripts {
     'config.lua',
 }
 
-author 'MaDHouSe'
-
-description 'Qb Drifting'
-
-version '1.0'
-
 client_script {
     'client/main.lua'
 }
 
 server_script {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+    'server/main.lua',
 }
